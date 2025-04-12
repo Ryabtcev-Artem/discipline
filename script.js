9d75b00612523fa3386ae3ac93675e169abed9ad
ryaba <ryabatema@gmail.com>
2025-04-12 12:39:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const timer = document.querySelector('.timer');
+const daysCountElement = document.querySelector('.timerSigns__daysCount')
+const hoursCountElement = document.querySelector('.timerSigns__hoursCount')
+const minutesCountElement = document.querySelector('.timerSigns__minutesCount')
+const secondsCountElement = document.querySelector('.timerSigns__secondsCount')
+const startTime = new Date(2025,3,12,11,3);
+
+setInterval(() => {
+    const currentTime = new Date();
+    const difference = Math.floor((currentTime - startTime) / 1000); // в секундах
+    const days = String(Math.floor(difference / 86400)).padStart(2, '0');
+    const hours = String(Math.floor((difference % 86400)/3600)).padStart(2, '0');
+    const minutes = String(Math.floor((difference % 3600) / 60)).padStart(2, '0');
+    const seconds = String(difference % 60).padStart(2, '0');
+    daysCountElement.innerText = days+':'
+    hoursCountElement.innerText = hours+':'
+    minutesCountElement.innerText = minutes+':'
+    secondsCountElement.innerText = seconds
+}, 1000);
