146731dfd8aa63e7ed2dce7c02f2254435a50467
ryaba <ryabatema@gmail.com>
2025-04-25 18:54:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const daysCountElement = document.querySelector('.timerSigns__daysCount')
 const hoursCountElement = document.querySelector('.timerSigns__hoursCount')
 const minutesCountElement = document.querySelector('.timerSigns__minutesCount')
 const secondsCountElement = document.querySelector('.timerSigns__secondsCount')
-const startTime = new Date(2025,3,24,14,40);
+const startTime = new Date(2025,3,25,18,30);
 
 setInterval(() => {
     const currentTime = new Date();
